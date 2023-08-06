07943d58f644d34c982c5e0fa94cac784c2b064d
Pkw <104768274+wowpkw@users.noreply.github.com>
2023-08-06 20:51:35 +0200

    Delete run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo    VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-echo    -------------------------------
-echo      Press enter to continue...
-echo    -------------------------------
-
-py main.py
-
