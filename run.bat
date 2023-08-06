d23e38f9fa01767d406243715de7b41908ce738d
Pkw <104768274+wowpkw@users.noreply.github.com>
2023-08-06 20:47:33 +0200

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+echo    VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+echo    -------------------------------
+echo      Press enter to continue...
+echo    -------------------------------
+
+py main.py
+
